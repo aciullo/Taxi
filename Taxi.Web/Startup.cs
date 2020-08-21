cc3dd4f82761e5894dbda1ce5d336d4715c2de1f
aciullo <andy.ciullos@gmail.com>
2020-08-20 23:18:01 -0400

    configuracion de usuarios y roles carga en bd de datos iniciales

--- Taxi.Web/Startup.cs
+++ Taxi.Web/Startup.cs
@@ -6,11 +6,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Taxi.Web.Data;
+using Taxi.Web.Data.Entities;
+using Taxi.Web.Helper;
 
 namespace Taxi.Web
 {
@@ -32,6 +35,16 @@ namespace Taxi.Web
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+           
+            services.AddIdentity<UserEntity, IdentityRole>(cfg =>
+            {
+                cfg.User.RequireUniqueEmail = true;
+                cfg.Password.RequireDigit = false;
+                cfg.Password.RequiredUniqueChars = 0;
+                cfg.Password.RequireLowercase = false;
+                cfg.Password.RequireNonAlphanumeric = false;
+                cfg.Password.RequireUppercase = false;
+            }).AddEntityFrameworkStores<DataContext>();
 
             //#REM se agrega para la conexion de base de datos
             services.AddDbContext<DataContext>(cfg =>
@@ -40,6 +53,7 @@ namespace Taxi.Web
             });
 
             services.AddTransient<SeedDb>();
+            services.AddScoped<IUserHelper, UserHelper>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -59,6 +73,7 @@ namespace Taxi.Web
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
